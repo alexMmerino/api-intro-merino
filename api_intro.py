43fc0444aed58e63f7766fa2e64ca1fc11355d1a
alexMmerino <acrimin35@gmail.com>
2022-11-14 10:53:42 -0800

    Updating files per new change requests.

--- api_intro.py
+++ api_intro.py
@@ -1,24 +1,31 @@
 from datetime import date
+import json
 
 import requests
 
-import config
-
-# Client IDs and Secrets from config.py
+# Get configuration data from config.json
 try:
-    OB_CLIENT_ID = config.OB_CLIENT_ID
-    OB_CLIENT_SECRET = config.OB_CLIENT_SECRET
-    SN_CLIENT_ID = config.SN_CLIENT_ID
-    SN_CLIENT_SECRET = config.SN_CLIENT_SECRET
-    BASE_URL = config.BASE_URL
-except AttributeError:
+    with open("config.json", "r") as config_file:
+        config_data = json.load(config_file)
+        config_file.close()
+except FileNotFoundError:
+    raise Exception('Config.json file is missing.')
+
+# load configuration constants
+try:
+    OB_CLIENT_ID = config_data['OB_CLIENT_ID']
+    OB_CLIENT_SECRET = config_data['OB_CLIENT_SECRET']
+    SN_CLIENT_ID = config_data['SN_CLIENT_ID']
+    SN_CLIENT_SECRET = config_data['SN_CLIENT_SECRET']
+    BASE_URL = config_data['BASE_URL']
+except KeyError:
     raise Exception('Config file is missing configuration.')
 
 
 # API urls
-svcnow_url = BASE_URL + 'v1/servicenow/colleges'
-onbase_url = BASE_URL + 'v2/onbase-docs/documents'
-auth_url = BASE_URL + 'oauth2/token'
+svcnow_url = f'{BASE_URL}v1/servicenow/colleges'
+onbase_url = f'{BASE_URL}v2/onbase-docs/documents'
+auth_url = f'{BASE_URL}oauth2/token'
 
 
 def get_access_token(key, secret):
@@ -63,18 +70,18 @@ if __name__ == '__main__':
     print('API Call 1: Service Now API Retrieve College Data')
     college_data = get_api_response(sn_access_token, svcnow_url)
     for college in college_data[:5]:
-        print(f"- College Code: {college['attributes']['code']}" +
+        print(f"- College Code: {college['attributes']['code']}"
               f", College Name: {college['attributes']['description']}")
 
     # API Call 2: Get Onbase Documents
     print('API Call 2: Retrieve Onbase Documents')
     end_date = str(date.today())
-    url_filter = (f'?filter[documentTypeName]=ADMS Deny&filter[endDate]=' +
+    url_filter = (f'?filter[documentTypeName]=ADMS Deny&filter[endDate]='
                   f'{end_date}')
     document_data = get_api_response(ob_access_token, onbase_url + url_filter)
     for document in document_data[:5]:
-        print(f"- Doc ID: {document['id']}, Doc Name: " +
-              f"{document['attributes']['name']}, Created Date: " +
+        print(f"- Doc ID: {document['id']}, Doc Name: "
+              f"{document['attributes']['name']}, Created Date: "
               f"{document['attributes']['storedDate']}")
         doc_url = f"/{document['id']}/keywords"
 
@@ -82,9 +89,9 @@ if __name__ == '__main__':
         keyword_data = get_api_response(ob_access_token, onbase_url + doc_url)
         keywords = keyword_data['attributes']['keywords']
         for keyword in keywords:
-            if keyword['name'] == 'ADMS - Application Term':
-                print('-- Application Term: ' +
-                      f"{keyword['values'][0]['formattedValue']}")
-            if keyword['name'] == 'ADMS - App Number':
-                print('-- Application Number: ' +
-                      f"{keyword['values'][0]['formattedValue']}")
+            if keyword['values']:
+                values = keyword['values'][0]
+                if keyword['name'] == 'ADMS - Application Term':
+                    print(f"-- Application Term: {values['formattedValue']}")
+                if keyword['name'] == 'ADMS - App Number':
+                    print(f"-- Application Number: {values['formattedValue']}")
